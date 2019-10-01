4a3889e4b591bdd8d4941be49c5e0e2348d501b6
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-01 02:08:27 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,4 @@
 module PostsHelper
-  module PostsHelper
   def author_id_field(post)
     if post.author.nil?
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
